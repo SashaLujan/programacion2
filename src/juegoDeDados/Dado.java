8d26b769ede1a105c7abd21fa1b69e97b45c2662
SashaLujan <56403549+SashaLujan@users.noreply.github.com>
2024-12-01 20:58:15 -0300

    Add files via upload

---
+++ src/juegoDeDados/Dado.java
@@ -0,0 +1,32 @@
+package juegoDeDados;
+
+public class Dado {
+   int caras;
+   
+   public Dado(int c) {
+	   this.setCaras(c);
+   }
+   public Dado() {
+	   this(6);
+   }
+   public void setCaras(int x) {
+	   if (x>0) {
+		   caras = x;
+	   } else {
+		   if (x<0) {
+			   caras = -x;
+		   } else {
+			   caras = 2;
+		   }
+	   }
+   }
+   
+   public int getCaras() {
+	   return caras;
+	   
+   }
+   
+   public int tirar() {
+	   return (int)((Math.random()*this.caras)+1);
+   }
+}
